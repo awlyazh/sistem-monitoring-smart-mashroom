bdcdc9c59d3d2b056488e5c8d70aafd945f764eb
SherlySandrina <sherlysandrina23@mhs.politala.ac.id>
2025-04-10 15:34:08 +0800

    membuat fitur profil

---
+++ app/src/main/java/com/example/smartmashroom/PengembunanActivity.java
@@ -0,0 +1,12 @@
+package com.example.smartmashroom;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PengembunanActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pengembunan);
+    }
+}
