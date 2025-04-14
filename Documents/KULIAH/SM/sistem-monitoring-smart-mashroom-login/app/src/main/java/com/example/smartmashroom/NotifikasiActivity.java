543f8b392e507ba6660bcfaf1cc0869d1a236be8
Intan <nurintanqolbinuri23@politala.ac.id>
2025-04-14 12:01:52 +0800

    Update .gitignore and resolve line endings

---
+++ Documents/KULIAH/SM/sistem-monitoring-smart-mashroom-login/app/src/main/java/com/example/smartmashroom/NotifikasiActivity.java
@@ -0,0 +1,12 @@
+package com.example.smartmashroom;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NotifikasiActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifikasi);
+    }
+}
